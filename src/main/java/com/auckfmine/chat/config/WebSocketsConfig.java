7eee56ac54b55391381cff83b6f1d9c63fc327ad
off-agency <mouhamedaminerouatbi@gmail.com>
2022-02-21 00:52:23 +0000

    changes

--- src/main/java/com/auckfmine/chat/config/WebSocketsConfig.java
+++ src/main/java/com/auckfmine/chat/config/WebSocketsConfig.java
@@ -25,7 +25,7 @@ public class WebSocketsConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws")
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
